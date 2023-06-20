37257d29836d5a1e73b73621ce4fe0dd63d2e9f0
Joshua Frías <metatron1495@gmail.com>
2023-06-19 22:27:20 -0600

    Add: Introducción a herencia y limpieza de código

--- src/TestReferencia2.java
+++
@@ -1,7 +0,0 @@
-public class TestReferencia2 {
-    public static void main(String[] args) {
-        Cuenta cuentaDiego = new Cuenta();
-        
-        System.out.println(cuentaDiego.getTitular().getNombre());
-    }
-}
